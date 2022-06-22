d9fb3a22b9d0dd0a0bb1b8d279b4d132d7324cef
AndrSilvestrov <91902694+AndrSilvestrov@users.noreply.github.com>
2022-06-22 15:07:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// let name = "Генератор захисного поля";
+// let price = 1000;
+
+// console.log("Обрано " + name + ", ціна за штуку " + price + " кредитів.");
+
+// price = 2000;
+
+// console.log("Обрано " + name + ", ціна за штуку " + price + " кредитів.");
+
+// let str = "привіт!";
+
+// let num = 123;
+
+// let flag = true;
+
+// let txt = "true";
+
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
